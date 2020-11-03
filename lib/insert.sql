498afd92537cd0e6d7545be8ebb330feef13599c
adnapp <adam.napps@gmail.com>
2020-11-03 15:38:56 -0500

    commit tree 72bd39add02cd6b4461dfa8a61fd2dfd7da196af

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ("Mr. Chocolate", 29, "M", "Brown", "angry", 1);
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ("Rowdy", 21, "M", "Brown", "nice", 1);
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ("Tabitha", 39, "M", "Black", "sleepy", 1);
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ("Sergeant Brown", 29, "M", "Brown", "nice", 1);
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ("Melissa", 59, "M", "Brown", "nice", 1);
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ("Grinch", 34, "M", "Black", "sleepy", 1);
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ("Wendy", 54, "M", "Polar", "nice", 1);
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES (NULL, 20, "M", "Brown", "happy", 1);
